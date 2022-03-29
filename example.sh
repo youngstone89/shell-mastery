77879c9c5dec26f4638960666a231516df10e234
YeongSeok Kim <youngstone89@icloud.com>
2022-03-29 20:24:59 +0900

    added zip

---
+++ example.sh
@@ -0,0 +1,5 @@
+zip -r archive_name.zip folder_to_compress
+unzip archive_name.zip
+zip -r -X archive_name.zip folder_to_compress
+tar -zcvf archive_name.tar.gz folder_to_compress
+tar -zxvf archive_name.tar.gz
