3de22534b59c53dd2010b00e4495990ca57d1fda
YeongSeok Kim <youngstone89@icloud.com>
2022-05-28 00:27:52 +0900

    added exclude .git for zip

--- example.sh
+++ example.sh
@@ -5,3 +5,6 @@ tar -zcvf archive_name.tar.gz folder_to_compress
 tar -zxvf archive_name.tar.gz
 zip -r ./event-log-collector-v0.2.2-without-cgo.zip event-log-collector-v0.2.2
 zip -r ./event-log-collector-v0.2.4-without-cgo.zip event-log-collector-v0.2.4
+
+zip -r solid-golang-class_week1.zip solid-golang-class
+zip -x '*.git*' -r ~/Downloads/solid-golang-class_week1.zip solid-golang-class
